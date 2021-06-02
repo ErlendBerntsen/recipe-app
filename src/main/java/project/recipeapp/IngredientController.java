03d274a0e33ffe8239dee903c7330df586b1d11d
Erlend <Erlend.Berntsen@student.uib.no>
2021-06-03 00:24:47 +0200

    Added patch operation to the ingredient controller. Still need to write some more tests for the controller

--- src/main/java/project/recipeapp/IngredientController.java
+++ src/main/java/project/recipeapp/IngredientController.java
@@ -4,10 +4,12 @@ import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
@@ -24,7 +26,7 @@ public class IngredientController {
     }
 
     @GetMapping("/ingredients")
-    CollectionModel<EntityModel<Ingredient>> all(){
+    public CollectionModel<EntityModel<Ingredient>> all(){
         List<EntityModel<Ingredient>> ingredients = repository.findAll().stream()
                 .map(assembler::toModel)
                 .collect(Collectors.toList());
@@ -33,7 +35,7 @@ public class IngredientController {
     }
 
     @PostMapping("/ingredients")
-    ResponseEntity<?> newIngredient(@RequestBody Ingredient ingredient){
+    public ResponseEntity<?> newIngredient(@RequestBody Ingredient ingredient){
         EntityModel<Ingredient> entityModel = assembler.toModel(repository.save(ingredient));
         return ResponseEntity
                 .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
@@ -41,15 +43,33 @@ public class IngredientController {
     }
 
     @GetMapping("/ingredients/{id}")
-    EntityModel<Ingredient> one(@PathVariable Long id){
-        Ingredient ingredient = repository.findById(id).orElseThrow(() -> new IngredientNotFoundException(id));
+    public EntityModel<Ingredient> one(@PathVariable Long id){
+        var ingredient = repository.findById(id).orElseThrow(() -> new IngredientNotFoundException(id));
         return assembler.toModel(ingredient);
     }
 
 
     @DeleteMapping("/ingredients/{id}")
-    ResponseEntity<?> deleteIngredient(@PathVariable Long id) {
+    public ResponseEntity<?> deleteIngredient(@PathVariable Long id) {
         repository.deleteById(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/ingredients/{id}")
+    public ResponseEntity<?> editIngredient(@PathVariable Long id, @RequestBody Map<String, Object> newFields) {
+        var ingredient = repository.findById(id).orElseThrow(() -> new IngredientNotFoundException(id));
+        newFields.forEach((key, value) -> {
+            var field = ReflectionUtils.findField(Ingredient.class, key);
+            assert field != null;
+            field.setAccessible(true);
+            ReflectionUtils.setField(field, ingredient, value);
+            field.setAccessible(false);
+
+        });
+
+        EntityModel<Ingredient> entityModel = assembler.toModel(repository.save(ingredient));
+        return ResponseEntity
+                .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
+                .body(entityModel);
     }
 }
--- src/test/java/project/recipeapp/IngredientControllerTest.java
+++ src/test/java/project/recipeapp/IngredientControllerTest.java
@@ -7,14 +7,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import project.recipeapp.units.Unit;
 import project.recipeapp.units.volumes.CentiLiter;
+import project.recipeapp.units.volumes.Liter;
 import project.recipeapp.units.weights.Gram;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -23,7 +30,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class IngredientControllerTest {
+class IngredientControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -133,6 +140,87 @@ public class IngredientControllerTest {
                 .andDo(print())
                 .andExpect(status().isNotFound());
     }
+
+    @Test
+    void ingredientShouldBeEditedByController(){
+        ingredientController.newIngredient(ingredient);
+        String newName = "new name";
+        double newPrice = 100;
+        Map<String, Object> newFields = new HashMap<>();
+        newFields.put("name", newName);
+        newFields.put("price", newPrice);
+
+
+        assertTrue(ingredientRepository.findById(ingredient.getId()).isPresent());
+        assertNotEquals(newName, ingredientRepository.findById(ingredient.getId()).get().getName());
+        assertNotEquals(newPrice, ingredientRepository.findById(ingredient.getId()).get().getPrice());
+
+        ingredientController.editIngredient(ingredient.getId(), newFields);
+
+        assertTrue(ingredientRepository.findById(ingredient.getId()).isPresent());
+        assertEquals(newName, ingredientRepository.findById(ingredient.getId()).get().getName());
+        assertEquals(newPrice, ingredientRepository.findById(ingredient.getId()).get().getPrice());
+    }
+
+    @Test
+    void ingredientChangeEveryFieldByController(){
+        ingredientController.newIngredient(ingredient);
+        String newName = "new name";
+        double newPrice = 100;
+        double newAmount = 1;
+        Unit newUnit = new Liter();
+        unitRepository.save(newUnit);
+        Category newCategory = Category.VODKA;
+
+        Map<String, Object> newFields = new HashMap<>();
+        newFields.put("name", newName);
+        newFields.put("price", newPrice);
+        newFields.put("amount", newAmount);
+        newFields.put("unit", newUnit);
+        newFields.put("category", newCategory);
+
+
+        assertTrue(ingredientRepository.findById(ingredient.getId()).isPresent());
+        assertNotEquals(newName, ingredientRepository.findById(ingredient.getId()).get().getName());
+        assertNotEquals(newPrice, ingredientRepository.findById(ingredient.getId()).get().getPrice());
+        assertNotEquals(newAmount, ingredientRepository.findById(ingredient.getId()).get().getAmount());
+        assertNotEquals(newUnit.getName(), ingredientRepository.findById(ingredient.getId()).get().getUnit().getName());
+        assertNotEquals(newCategory, ingredientRepository.findById(ingredient.getId()).get().getCategory());
+
+        ingredientController.editIngredient(ingredient.getId(), newFields);
+
+        assertTrue(ingredientRepository.findById(ingredient.getId()).isPresent());
+        assertEquals(newName, ingredientRepository.findById(ingredient.getId()).get().getName());
+        assertEquals(newPrice, ingredientRepository.findById(ingredient.getId()).get().getPrice());
+        assertEquals(newAmount, ingredientRepository.findById(ingredient.getId()).get().getAmount());
+        assertEquals(newUnit.getName(), ingredientRepository.findById(ingredient.getId()).get().getUnit().getName());
+        assertEquals(newCategory, ingredientRepository.findById(ingredient.getId()).get().getCategory());
+    }
+
+    @Test
+    void ingredientShouldBeEditedByRequest() throws  Exception{
+        ingredientController.newIngredient(ingredient);
+        String newName = "new name";
+        String body = "\"name\":\"" + newName + "\"";
+        String jsonEdit = "{" + body + "}";
+
+
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.patch("/ingredients/" + ingredient.getId())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content(jsonEdit);
+
+        this.mockMvc.perform(builder)
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(content().string(containsString(body)));
+
+    }
+    //TODO Make above test handle more edits to a recipe
+    //TODO Make tests with mock GET requests for one/all
+    //TODO Make test for mock POST request
 
 
 
